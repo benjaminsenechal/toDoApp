8921884b7eab63dd0412bb13f649509d12593038
benjaminsenechal <benjamin.senechal1@epsi.fr>
2014-01-27 10:22:45 +0100

    Initialize app

---
+++ toDoAppTests/toDoAppTests.m
@@ -0,0 +1,34 @@
+//
+//  toDoAppTests.m
+//  toDoAppTests
+//
+//  Created by Benjamin SENECHAL on 27/01/2014.
+//  Copyright (c) 2014 Benjamin SENECHAL. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface toDoAppTests : XCTestCase
+
+@end
+
+@implementation toDoAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
